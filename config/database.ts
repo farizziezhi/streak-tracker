6b3c4dfb1f4547dcbb2534d3d980e4337588fa7e
muh.farizzi <ezhifarizzi@gmail.com>
2025-10-31 23:25:17 +0800

    Update database config for Railway MySQL variables

--- config/database.ts
+++ config/database.ts
@@ -7,11 +7,11 @@ const dbConfig = defineConfig({
     mysql: {
       client: 'mysql2',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
+        host: env.get('MYSQLHOST') || env.get('DB_HOST'),
+        port: env.get('MYSQLPORT') || env.get('DB_PORT'),
+        user: env.get('MYSQLUSER') || env.get('DB_USER'),
+        password: env.get('MYSQLPASSWORD') || env.get('DB_PASSWORD'),
+        database: env.get('MYSQLDATABASE') || env.get('DB_DATABASE'),
       },
       migrations: {
         naturalSort: true,
